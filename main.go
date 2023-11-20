752b2b071eeafeae60af4c9a3c8a3ef4e6e5d486
João Antônio Gomes <devjoaoantoniogomes@gmail.com>
2023-11-20 19:22:01 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/joao-antonio-gomes/go-gin-rest-api/routes"
+)
+
+func main() {
+	routes.HandleRequests()
+}
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joao-antonio-gomes/go-gin-rest-api/controllers"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+
+	r.GET("/students", controllers.ShowAllStudents)
+
+	r.Run()
+}
